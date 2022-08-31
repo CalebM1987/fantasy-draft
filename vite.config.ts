47c86feb33e5c90a68c85e59ff45cde47d72ae02
CalebM1987 <caleb.mackey@gmail.com>
2022-08-31 00:10:14 -0500

    chore: vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
+  resolveAlias: {
     '/@/': path.resolve(__dirname, './src')
   },
   // resolve: {
