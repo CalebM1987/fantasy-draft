9d2284596cbe1f977ca276cc1e744218eeb7c7f5
CalebM1987 <caleb.mackey@gmail.com>
2022-08-31 01:42:28 -0500

    refactor(firebase): use .env file to pass firebase info

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -23,13 +23,13 @@ import { log } from "../utils/logger";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAu7eqVMUTBCR5tQaHbi-nR41CkdiHi7M8",
-  authDomain: "fantasy-football-draft-d858f.firebaseapp.com",
-  projectId: "fantasy-football-draft-d858f",
-  storageBucket: "fantasy-football-draft-d858f.appspot.com",
-  messagingSenderId: "948347030107",
-  appId: "1:948347030107:web:3b45426b32a32244518d7e",
-  measurementId: "G-2HMTFW2L45"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
