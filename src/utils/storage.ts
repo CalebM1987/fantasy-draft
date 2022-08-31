d5f34f460532de451357ff76d932c2784be1cd5e
CalebM1987 <caleb.mackey@gmail.com>
2022-08-30 22:54:11 -0500

    feat(utils): add option to return default value in readFromStorage()

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,12 +4,18 @@ export function saveToStorage(key: string, obj: any) {
   }
 }
 
-export function loadFromStorage<T>(key: string): T | undefined {
+/**
+ * load an item from local storage
+ * @param key - the local storage lookup key
+ * @param def - an optional default value if nothing is retrieved from local storage
+ * @returns 
+ */
+export function loadFromStorage<T>(key: string, def?: T): T | undefined {
   if (localStorage){
     const item = localStorage.getItem(key)
     if (item){
       return JSON.parse(item) as T
     }
   }
-  return 
+  return def
 }
