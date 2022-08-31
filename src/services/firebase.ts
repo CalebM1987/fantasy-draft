690d7cf7afda2ba35516d2fbd31bd75c5a88846d
CalebM1987 <caleb.mackey@gmail.com>
2022-08-31 12:44:40 -0500

    chore: include project id in plaintext

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -25,7 +25,8 @@ import { log } from "../utils/logger";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  // projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: 'fantasy-football-draft-d858f',
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
