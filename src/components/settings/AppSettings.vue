87b20e9fcc1cd64bf388c5069d30c9e05bcd19d6
CalebM1987 <caleb.mackey@gmail.com>
2022-09-02 22:59:14 -0500

    chore: started app settings component

---
+++ src/components/settings/AppSettings.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useAppStore } from '../../store';
+const appState = useAppStore()
+
+</script>
+
+<template>
+  <div class="app-settings q-pa-md">
+    <div class="text-h5">App Settings</div>
+    <q-separator class="q-my-md" />
+
+    <div class="settings">
+      <q-toggle
+        v-model="appState.compactView"
+        label="Compact View"
+      />
+    </div>
+  </div>
+
+</template>
