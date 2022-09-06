6b9577695424a726554ad09db22979c630fe4d43
CalebM1987 <caleb.mackey@gmail.com>
2022-09-06 15:51:53 -0500

    feat: helper function to get player headshot

--- src/utils/espn.ts
+++ src/utils/espn.ts
@@ -92,3 +92,8 @@ export function copyESPNUpdateRostersFunction() {
   const rosters = getRosters()
   return copyToClipboard(templateFunction(rosters))
 }
+
+export function getHeadshot(playerOrId: IEspnPlayer | number): string {
+  const pid = typeof playerOrId === 'number' ? playerOrId: playerOrId.id
+  return `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${pid}.png&w=96&h=70&cb=1`
+}
